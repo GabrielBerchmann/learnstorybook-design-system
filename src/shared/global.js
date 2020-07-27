87a26884a54bd65fe42e5ba903bf8f261a62382f
gbn <gbn@sybit.de>
2020-07-27 16:56:02 +0200

    Storybook deployment with GitHub action

---
+++ src/shared/global.js
@@ -0,0 +1,16 @@
+// src/shared/global.js
+
+import { createGlobalStyle, css } from 'styled-components';
+import { color, typography } from './styles';
+
+export const fontUrl = 'https://fonts.googleapis.com/css?family=Nunito+Sans:400,700,800,900';
+
+export const bodyStyles = css`
+  /* same as before */
+`;
+
+export const GlobalStyle = createGlobalStyle`
+ body {
+   ${bodyStyles}
+ }
+`;
